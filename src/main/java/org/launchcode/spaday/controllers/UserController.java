26291fdd1b69a0c6ba59d792ca10e96cb27a2944
leerohrer1 <loginlee3@gmail.com>
2022-10-12 19:52:11 -0500

    some work is done

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,17 @@
+package org.launchcode.spaday.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/user/add")
+    public String displayAddUserForm(@RequestParam ) {
+        return "user/add";
+    }
+
+}
