f019465014cb7d14e94b77a95e2b49a7677a53bf
leerohrer1 <loginlee3@gmail.com>
2022-10-12 20:27:48 -0500

    some more work is done

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,17 +1,25 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/user/add")
-    public String displayAddUserForm(@RequestParam ) {
+    public String displayAddUserForm(Model model, @ModelAttribute User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("password", user.getPassword());
         return "user/add";
     }
 
+    @PostMapping("/user/add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        // add form submission handling code here
+        return "user/add";
+    }
 }
